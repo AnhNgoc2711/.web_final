9cecb0fcbba581feabaae00d70cf593d7766e3ef
NguyenChiVy2005 <nguyenchivy568@gmail.com>
2025-06-02 14:00:48 +0700

    Code 2/6 Not SETTING AND PERSONAL INFOR

--- js/login.js
+++ js/login.js
@@ -5,3 +5,18 @@ function showTab(formId, event) {
   // Hiện form được chọn
   document.getElementById(formId).classList.add('active');
 }
+// Ẩn/hiện Password
+document.querySelectorAll('.toggle-password').forEach(icon => {
+  icon.addEventListener('click', () => {
+    const input = icon.previousElementSibling;
+    if (input.type === 'password') {
+      input.type = 'text';
+      icon.classList.remove('bi-eye-slash');
+      icon.classList.add('bi-eye');
+    } else {
+      input.type = 'password';
+      icon.classList.remove('bi-eye');
+      icon.classList.add('bi-eye-slash');
+      }
+  });
+});
